e447e84ddf1a85b9a10259f41b6fc2d98ab394c6
megas89 <megas89@ukr.net>
2016-09-02 12:05:55 +0300

    finish multi lng switcher, they is sad, but it works

--- js/mult.js
+++ js/mult.js
@@ -1,9 +1,28 @@
-//global area
-function swapLng(x) {
-  var radioName = document.getElementsByName(x.name);
+//concat rulezzz!
+function swapLng(l) {
+  var radioName = document.getElementsByName(l.name);
   for (i = 0; i < radioName.length; i++) {
-    console.log(radioName.length)
     document.getElementById(radioName[i].id.concat("Setting")).style.display = "none";
+    }
+    document.getElementById(l.id.concat("Setting")).style.display = "block";
+}
+
+function readLng(){
+  var curLng = localStorage.getItem("lang");//read saved value from ls
+  var radioBtnSelector = document.querySelector('input[type="radio"][id=' + curLng + "]");//getElementsByName.checked dostn work((, so i use strange methods
+  radioBtnSelector.click();// magick ivent! love you!
+  radioBtnSelector.setAttribute('checked', 'checked');
+  console.log(curLng, radioBtnSelector.value);
+};
+
+function saveLng() {
+ var getRadioButtons = document.getElementsByName("lang");
+  for (var x = 0; x < getRadioButtons.length; x ++) {
+    if (getRadioButtons[x].checked) {
+      console.log("You checked " + getRadioButtons[x].id);
+      localStorage.setItem("lang", getRadioButtons[x].id);
+      break;
+    }
+
   }
-  document.getElementById(x.id.concat("Setting")).style.display = "block";
 }
